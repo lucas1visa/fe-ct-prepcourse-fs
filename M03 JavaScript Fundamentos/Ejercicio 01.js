2539f3af1cd1488dd4ccbdec7b626aae2ae52e31
lucas1visa <visalucas557@gmail.com>
2023-03-07 18:15:58 +0100

    Homework M03 JavaScript

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,7 +5,7 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = Hola;
+const nuevoString = "Hola" ;
 
 // Crea una variable de tipo number.
 const nuevoNumero = 12;
